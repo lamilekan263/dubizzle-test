753615365789507fd5949a395cb6a85dd59a1621
lamilekan263 <lamilekan263@gmail.com>
2023-04-05 12:59:24 +0100

    feat: Gist list component done

--- src/components/GistList.js
+++ src/components/GistList.js
@@ -1,4 +1,30 @@
-import React, { useEffect, useState } from 'react'
-const GistList = () => <></>
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
 
-export default GistList
+import Gist from "./Gist";
+
+const GistWrapper = styled.div`
+  width: 60%;
+  margin-left: auto;
+  margin-right: auto;
+  @media (max-width: 768px) {
+    width: 100%;
+    padding: 20px;
+  }
+`;
+
+function GistList ({ gists }) {
+    return (
+        <GistWrapper>
+            { gists?.length > 0 &&
+                gists.map((gist) => <Gist key={ gist.id } gist={ gist } />) }
+        </GistWrapper>
+    );
+}
+
+
+GistList.propTypes = {
+    gists: PropTypes.array,
+};
+export default GistList;
