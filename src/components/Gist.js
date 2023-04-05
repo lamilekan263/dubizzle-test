86756b0871e19dc1e89d6e2fa58bbdf8465e1aa0
lamilekan263 <lamilekan263@gmail.com>
2023-04-05 12:57:14 +0100

    feat: Gist component

--- src/components/Gist.js
+++ src/components/Gist.js
@@ -1,3 +1,79 @@
-const Gist = ({ gist }) => <>
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import Octicon from "react-octicon";
+// custom imports
+import GistHeader, { Link } from "./GistHeader";
 
-export default Gist
+const Gist = ({ gist }) => {
+    // get owner/author data of the gist
+    const { owner: gist_owner } = gist;
+    // get the meta data of the gist
+    const {
+        description = "",
+        created_at,
+        updated_at,
+        files = {},
+        comments = 0,
+        forks_url,
+        comments_url,
+        html_url = "#",
+    } = gist;
+    // retrieve only File Names from the object of files
+    const fileNames = Object.keys(files);
+
+    return (
+        <>
+            <GistHeader
+                gist_owner={ gist_owner }
+                gist_meta={ {
+                    created_at,
+                    updated_at,
+                    files,
+                    comments,
+                    comments_url,
+                    forks_url,
+                    html_url,
+                } }
+            />
+            <GistDescription>{ description }</GistDescription>
+            <GistFiles>
+                { fileNames.map((f) => {
+                    return (
+                        <Link href={ files[f].raw_url } target="new" key={ f }>
+                            <Octicon name="file">{ f }</Octicon>
+                        </Link>
+                    );
+                }) }
+            </GistFiles>
+            <Separator />
+        </>
+    );
+};
+
+const GistDescription = styled.p`
+  padding-left: 15px;
+  width: 100%;
+  font-size: 17px;
+`;
+
+const GistFiles = styled.div`
+  display: inline-block;
+  color: #0366d6;
+  width: 100%;
+  margin-left: 20px;
+  margin-bottom: 20px;
+  & > a {
+    margin-right: 15px;
+    padding: 5px;
+  }
+`;
+const Separator = styled.hr`
+  border;0.5px solid lightgray;
+`;
+//Props Param Type Checking
+Gist.propTypes = {
+    gist: PropTypes.object.isRequired,
+};
+
+export default Gist;
